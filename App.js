3871824188f9bc1ca4c0518ecd44a8836475f0a4
Renata Volkonovskaya <volenata4@gmail.com>
2025-04-28 20:46:19 +0300

    writed all auth methods with necessary sequels

--- App.js
+++ App.js
@@ -1,56 +1,100 @@
 console.log("Server started")
 
-const temp = [
-    {
-        id: 1,
-        username: "username",
-        data: "13.04.2005 13:40",
-        edited: false,
-        message: "some text",
-    },
-    {
-        id: 2,
-        username: "user nickname",
-        data: "27.10.2005 5:13",
-        edited: true,
-        message: "ttteeexxxtttsdfgdfghydfgh dfghuidfghseo88gshgs 87ghsogsdhuighsdg uisrtytvwe78obtywenv78gttteeexxxttts dfgdfghydfghdfghuidfghseo88gshgs87 ghsogsdhuighsdguisrtytvwe78obtywenv78gttteeexxxtttsdfgdfg hydfghdfghuidfghseo88gshgs87ghsogsdhuighsdguisrtytvwe78obtyw env78gttteeexxxtttsdfgdfghydfghdfghuidfghseo88gshgs87ghsogsdhui fgdgsdgdfshdgsdzghjyfghfhdgsfgshdjgnfsdhtjgnbvfrthfhjngfbvdfrthfjgnvbcdfsrgthfgbcvsdfghfgfddghfghigushervteroisuvgnhser8buigebshdvguiesdhvgndsfyuigvhnsrbt87seruivghni ghsdguisrtytvwe78obtywenv78gttteeexxxtttsdfgdfghydfghdfghuidfghseo88gshgs87ghsogsdhuighsdguisrtytvwe78obtywenv78gttteeexxxtttsdfgdfghydfghdfghuidfghseo88gshgs87ghsogsdhuighsdguisrtytvwe78obtywenv78gttteeexxxtttsdfgdfghydfghdfghuidfghseo88gshgs87ghsogsdhuighsdguisrtytvwe78obtywenv78gttteeexxxtttsdfgdfghydfghdfghuidfghseo88gshgs87ghsogsdhuighsdguisrtytvwe78obtywenv78gttteeexxxtttsdfgdfghydfghdfghuidfghseo88gshgs87ghsogsdhuighsdguisrtytvwe78obtywenv78gttteeexxxtttsdfgdfghydfghdfghuidfghseo8 8gshgs87ghsogsdhuighsdguisrtytvwe78obtywenv78gttteeexxxtttsdfgdfghydfghdfghuidfghseo88gshgs87ghsogsdhuighsdguisrtytvwe78obtywenv78gttteeex xxtttsdfgdfghydfghdfghuidfghseo88gshgs87ghsogsdhuighsdguisrtytvwe78obtywenv78g",
-    },
-    {
-        id: 3,
-        username: "my login is bad",
-        data: "29.01.2017 13:40",
-        edited: false,
-        message: "my message is s beatifull)))",
-    },
-    {
-        id: 4,
-        username: "user nickname",
-        data: "29.01.2017 13:42",
-        edited: false,
-        message: "test",
-    },
-]
+hashPassword = (password) => {
+    return crypto.createHash('sha256').update(password).digest('hex');
+}
+
+const convertMessagesFromDBToJSON = (dbMessages) => {
+    let messages = []
+    dbMessages.forEach(dbMessage => {
+        try {
+            let message = {
+                id: dbMessage.id,
+                username: dbMessage.username,
+                data: dbMessage.datetime,
+                edited: dbMessage.is_edited,
+                message: dbMessage.message,
+            }
+            messages.push(message)
+        } catch (err) {
+            console.error(`Ошибка парсинга массива сообщений, полученных из БД, в JSON. Ошибка: ${err}`)
+            throw err
+        }
+    });
+    return messages
+}
 
 const WebSocket = require('ws');
+const { Pool } = require('pg');
+const crypto = require('crypto');
+// const dbRequests = require('./dbRequests')
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'messenger',
+    password: 'Jfjfj12j',
+    port: 5432,
+});
+
+async function sendQueryToDB(query) {
+    try {
+        const res = await pool.query(query)
+        return res.rows
+    } catch (err) {
+        console.error(`Ошибка выполнения запроса: ${err}`)
+        throw err
+    }
+}
+
+async function sendQueryToDB(query, values) {
+    try {
+        const res = await pool.query(query, values)
+        return res.rows
+    } catch (err) {
+        console.error(`Ошибка выполнения запроса: ${err}`)
+        throw err
+    }
+}
+
+async function getMessages() {
+    const query = `SELECT 
+      m.id, 
+      m.datetime, 
+      m.is_edited, 
+      m.message, 
+      u.username
+    FROM public.messages m
+    JOIN public.users u ON m.user_id = u.id
+  `
+    const messagesDB = await sendQueryToDB(query)
+    const messages = convertMessagesFromDBToJSON(messagesDB)
+    return messages
+}
+
+
+
+
+
+
+
 const server = new WebSocket.Server({ host: "127.0.0.1", port: 8080 });
 
 server.on('connection', socket => {
-    socket.on('message', message => {
-        const response = getResponse(message, socket);
+    socket.on('message', async message => {
+        const response = await getResponse(message, socket);
         socket.send(response);
     });
 });
 
-function getResponse(messageString, socket) {
+async function getResponse(messageString, socket) {
     let response = {
         username: null,
         responseCode: null,
-        messages: temp,
     }
 
     try {
         message = JSON.parse(messageString)
-        console.log(message)
     } catch {
         response.responseCode = "request is not a JSON"
         return response
@@ -66,19 +110,69 @@ function getResponse(messageString, socket) {
     switch (message.type) {
         case "is authorised":
             response.responseCode = "ok"
-            if (socket.username != undefined) {
+            if (socket.username !== undefined) {
                 response.username = socket.username
+                response.messages = await getMessages()
             }
-            response.username = "user nickname"//
+            // response.username = "user nickname"//
             break;
         case "log in":
-
+            if (message.password && message.username) {
+                const password = hashPassword(message.password)
+                const query = `SELECT EXISTS (
+                                SELECT 1 
+                                FROM public.users 
+                                WHERE username = $1 AND password = $2
+                            )`
+                const values = [message.username, password]
+                const request = await sendQueryToDB(query, values)
+                if (request[0].exists) {
+                    response.responseCode = "ok"
+                    response.username = message.username
+                    response.messages = await getMessages()
+                    socket.username = message.username
+                } else {
+                    response.responseCode = `auth failed`
+                }
+            } else {
+                response.responseCode = `Required fields are missing. Request type: ${message.type}`
+            }
             break;
         case "log out":
-
+            socket.username = undefined
+            response.responseCode = "ok"
             break;
         case "register":
-
+            if (message.password && message.username) {
+                const query = `SELECT EXISTS (
+                                SELECT 1 
+                                FROM public.users 
+                                WHERE username = $1
+                            )`
+                const values = [message.username]
+                const request = await sendQueryToDB(query, values)
+                if (request[0].exists) {
+                    response.responseCode = `user with login "${message.username}" alredy exists`
+                } else {
+                    const password = hashPassword(message.password)
+                    const query = `INSERT INTO public.users (username, password)
+                                    VALUES ($1, $2)
+                                    RETURNING username
+                    `
+                    const values = [message.username, password]
+                    const request = await sendQueryToDB(query, values)
+                    if (request[0].username) {
+                        response.responseCode = "ok"
+                        response.username = request[0].username
+                        response.messages = await getMessages()
+                        socket.username = request[0].username
+                    } else {
+                        response.responseCode = "database failure"
+                    }
+                }
+            } else {
+                response.responseCode = `Required fields are missing. Request type: ${message.type}`
+            }
             break;
         case "send":
 
@@ -93,9 +187,12 @@ function getResponse(messageString, socket) {
             response.responseCode = "request type is incorrect"
             break;
     }
-    return JSON.stringify(response);
-}
 
+    const responseString = JSON.stringify(response)
+    console.log(`request: "${messageString}" \nresponse: "${responseString}"`)//
+
+    return responseString
+}
 
 // server.clients.forEach(client => {
 //     if (client.readyState === WebSocket.OPEN) {
